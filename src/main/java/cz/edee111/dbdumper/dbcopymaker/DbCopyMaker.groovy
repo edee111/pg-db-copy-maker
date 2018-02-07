f4c83b382f9d55a9d014d05806feb6663a4290b7
Eduard Tomek <eduard.tomek@artin.cz>
2018-02-07 10:07:01 +0100

    db copy maker improvements

--- src/main/java/cz/edee111/dbdumper/dbcopymaker/DbCopyMaker.groovy
+++ src/main/java/cz/edee111/dbdumper/dbcopymaker/DbCopyMaker.groovy
@@ -1,12 +1,13 @@
-package cz.edee111.dbdumper.dbcopymaker
+package cz.edee111.dbdumper
 
+import cz.edee111.dbdumper.dbcopymaker.DbConnectionDetails
 import groovy.sql.GroovyResultSet
 import groovy.sql.GroovyResultSetExtension
 import groovy.sql.Sql
 
 import java.sql.ResultSet
 
-class DbCopyMaker {
+class DbDumper {
 
   @SuppressWarnings("GroovyUnusedDeclaration")
   private static final DbConnectionDetails dbSrc = new DbConnectionDetails(
@@ -26,21 +27,14 @@ class DbCopyMaker {
       dbName: ""
   )
 
-  @SuppressWarnings("GroovyUnusedDeclaration")
-  private static final DbConnectionDetails dbSrcTest = new DbConnectionDetails(
-      username: DB_USER,
-      password: "masterkey",
-      host: "localhost",
-      port: "5432",
-      dbName: ""
-  )
-
   private static String DB_USER = ""
-  private static String DB_SCHEMA_PATTERN = ""
-
   private static boolean MAKE_PAUSE = true
-  private static int MAKE_PAUSE_SECONDS_VARIABLITY = 30
-  private static int MAKE_PAUSE_SECONDS_FIXED = 40
+  private static int MAKE_PAUSE_SECONDS_VARIABLITY = 17
+  private static int MAKE_PAUSE_SECONDS_FIXED = 9
+
+  private static long TABLE_SIZE_LIMIT = 12000000
+  private static String START_FROM_TABLE_NAME = null //"aggregation_product"
+  private static String PROCESS_SINGLE_TABLE_NAME = null;
 
   Sql sqlSrc
   Sql sqlDst
@@ -57,7 +51,7 @@ class DbCopyMaker {
 
   void copyData() {
     sqlDst.withTransaction {
-      def schemas = new GroovyResultSetExtension(sqlSrc.connection.metaData.getSchemas(null, DB_SCHEMA_PATTERN))
+      def schemas = new GroovyResultSetExtension(sqlSrc.connection.metaData.getSchemas(null, ''))
       schemas.eachRow { schema ->
         //noinspection GroovyAssignabilityCheck
         copySchema(schema.table_schem)
@@ -66,19 +60,37 @@ class DbCopyMaker {
   }
 
   void copySchema(String schema) {
-    def tables = new GroovyResultSetExtension(sqlSrc.connection.metaData.getTables(null, schema, '%', 'TABLE'))
-
-    tables.eachRow { table ->
-      //noinspection GroovyAssignabilityCheck
-      copyTable(table.table_schem, table.table_name)
+    if (PROCESS_SINGLE_TABLE_NAME != null) {
+      copyTable(schema, PROCESS_SINGLE_TABLE_NAME);
     }
+    else {
+      def tables = new GroovyResultSetExtension(sqlSrc.connection.metaData.getTables(null, schema, '%', 'TABLE'))
+      tables.eachRow { table ->
+        //noinspection GroovyAssignabilityCheck
+        copyTable(table.table_schem, table.table_name)
+      }
+    }
+
   }
 
+  private static boolean doReallyCopy = false
+
   void copyTable(String schema, String table) {
+    if (!START_FROM_TABLE_NAME || START_FROM_TABLE_NAME == table) {
+      doReallyCopy = true
+    }
+    if (!doReallyCopy) {
+      return
+    }
+    println("${schema}.${table} - start")
+    println("${schema}.${table} - drop Fk constrains")
     dropFkConstraints(schema, table)
+    println("${schema}.${table} - truncate")
     truncateTable(schema, table)
     doCopyTable(schema, table)
+    println("${schema}.${table} - move sequences")
     moveSequences(schema, table)
+    println("${schema}.${table} - end")
     makePause()
   }
 
@@ -108,25 +120,61 @@ class DbCopyMaker {
     columns.eachRow {
       columnNames << it.COLUMN_NAME
     }
-    sqlSrc.eachRow("SELECT * FROM ${schema}.${table}" as String) { GroovyResultSet row ->
-      copyRow(schema, table, row, columnNames)
+
+    def rowCount = sqlSrc.firstRow("SELECT COUNT(*) as ROW_COUNT FROM " + schema + '.' + table).get('ROW_COUNT') as Integer
+    if (columnNames.contains("id")) {
+      println "Original row count in table ${schema}.${table}: ${rowCount}."
+      if (rowCount > TABLE_SIZE_LIMIT) {
+        rowCount = TABLE_SIZE_LIMIT
+        println "Setting row count for table ${schema}.${table} to ${rowCount}."
+      }
     }
+
+
+    int i = 0
+    List<String> values = []
+    String columnNamesStr = prepareColumnNamesStr(columnNames)
+
+    sqlSrc.eachRow("SELECT * FROM ${schema}.${table} ${ columnNames.contains('id') ? "ORDER BY id DESC LIMIT ${TABLE_SIZE_LIMIT}" : ""} " as String) { GroovyResultSet row ->
+      values << prepareValues(row, columnNames)
+
+      if (i % 1000 == 0) {
+        copyRows(schema, table, values, columnNamesStr)
+
+        if (i % 5000 == 0) {
+          println("${schema}.${table} - ${(i * 100) / rowCount}%")
+
+          if (i % 200000 == 0) {
+            sqlDst.commit() // asi je zbytecny
+          }
+        }
+      }
+
+      i++
+    }
+    copyRows(schema, table, values, columnNamesStr)
   }
 
-  void copyRow(String schema, String table, row, List<String> columnNames) {
-    def values = columnNames.collect {
-      row[it] != null ? "'${safeStringValue(row[it] as String)}'" : null
-    }
-    .join(', ')
-
-    def columnNamesStr = columnNames.collect {
+  static String prepareColumnNamesStr(List<String> columnNames) {
+    return columnNames.collect {
       "${it}"
     }
     .join(', ')
+  }
 
-    def sqlInsert = "INSERT INTO ${schema}.${table} (${columnNamesStr}) VALUES (${values})"
-    println sqlInsert
-    sqlDst.execute(sqlInsert as String)
+  static String prepareValues(row, List<String> columnNames) {
+    return '(' + columnNames.collect {
+      row[it] != null ? "'${safeStringValue(row[it] as String)}'" : null
+    }
+    .join(', ') + ')'
+  }
+
+  void copyRows(String schema, String table, List<String> values, String columnNamesStr) {
+    if (values.size() > 0) {
+      def sqlInsert = "INSERT INTO ${schema}.${table} (${columnNamesStr}) VALUES " + values.join(', ')
+      sqlDst.execute(sqlInsert as String)
+      values.clear()
+    }
   }
 
   void moveSequences(String schema, String table) {
@@ -167,7 +215,7 @@ class DbCopyMaker {
   }
 
   static String safeStringValue(String value) {
-    return value.replaceAll("'", '"').replaceAll("`", '"')
+    return value.replaceAll("'", "''")
   }
 
   void prepareDbConnections(DbConnectionDetails src, DbConnectionDetails dst) {
@@ -188,6 +236,6 @@ class DbCopyMaker {
   }
 
   static void main(String[] args) {
-    new DbCopyMaker().run()
+    new DbDumper().run()
   }
 }
